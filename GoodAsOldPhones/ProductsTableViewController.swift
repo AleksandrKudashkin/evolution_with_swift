262163b874c15962c5a43d8208dc4485f1512821
Aleksandr Kudashkin <alex@kudashkin.pro>
2018-02-26 13:54:04 -0500

    Create 5 rows with custom heigt and embeded image and text

--- GoodAsOldPhones/ProductsTableViewController.swift
+++ GoodAsOldPhones/ProductsTableViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ProductsTableViewController: UITableViewController {
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+        cell.textLabel?.text = "Hello friend"
+        cell.imageView?.image = #imageLiteral(resourceName: "image-cell1")
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 
 }
